272a5b39fb2b58ba0596fa9861425e3053735467
Ola Kuszewska <olakuszewska1@gmail.com>
2023-10-14 12:07:12 +0200

    initial commit

---
+++ src/main/java/io/github/akuszewska/tennislessons/TennisLessonsApplication.java
@@ -0,0 +1,13 @@
+package io.github.akuszewska.tennislessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisLessonsApplication.class, args);
+	}
+
+}
