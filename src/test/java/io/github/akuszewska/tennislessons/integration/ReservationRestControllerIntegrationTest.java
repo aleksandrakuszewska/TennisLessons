603f3978bf223dcd34b9cb230196a090d75ecf15
Ola Kuszewska <olakuszewska1@gmail.com>
2023-11-08 13:25:32 +0100

    integration tests rest

--- src/test/java/io/github/akuszewska/tennislessons/integration/ReservationRestControllerIntegrationTest.java
+++ src/test/java/io/github/akuszewska/tennislessons/integration/ReservationRestControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package io.github.akuszewska.tennislessons.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.akuszewska.tennislessons.TennisLessonsApplication;
 import io.github.akuszewska.tennislessons.builder.CourseBuilder;
 import io.github.akuszewska.tennislessons.builder.ReservationBuilder;
@@ -9,6 +10,7 @@ import io.github.akuszewska.tennislessons.domain.Course;
 import io.github.akuszewska.tennislessons.domain.Reservation;
 import io.github.akuszewska.tennislessons.domain.Role;
 import io.github.akuszewska.tennislessons.domain.User;
+import io.github.akuszewska.tennislessons.dto.ReservationDTO;
 import io.github.akuszewska.tennislessons.repository.CourseRepository;
 import io.github.akuszewska.tennislessons.repository.ReservationRepository;
 import io.github.akuszewska.tennislessons.repository.RoleRepository;
@@ -28,10 +30,10 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static io.github.akuszewska.tennislessons.builder.ReservationBuilder.ADDITIONAL_RESERVATION_REMARKS;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -93,6 +95,92 @@ public class ReservationRestControllerIntegrationTest {
                 .andExpect(jsonPath("$[0].accepted", is(Boolean.TRUE)));
     }
 
+    @Test
+    @WithMockUser(username = "student", roles = "STUDENT")
+    public void getClientReservations_thenReturns200StatusCode() throws Exception {
+
+        createReservation(Boolean.TRUE);
+        createReservation(Boolean.FALSE);
+
+        mvc.perform(get(CONTROLLER_BASE_URL.concat("/client-all"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    @WithMockUser(username = "instructor", roles = "INSTRUCTOR")
+    public void getInstructorReservations_thenReturns200StatusCode() throws Exception {
+
+        Reservation reservation = createReservation(Boolean.FALSE);
+
+        when(authService.getCurrentLoggedInUser()).thenReturn(instructor);
+
+        mvc.perform(get(CONTROLLER_BASE_URL.concat("/instructor"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].reservationId", is(reservation.getId().intValue())))
+                .andExpect(jsonPath("$[0].additionalRemarks", is(reservation.getAdditionalRemarks())))
+                .andExpect(jsonPath("$[0].accepted", is(reservation.getAccepted())))
+                .andExpect(jsonPath("$[0].clientId", is(reservation.getClient().getId().intValue())))
+                .andExpect(jsonPath("$[0].courseId", is(reservation.getCourse().getId().intValue())));
+    }
+
+    @Test
+    @WithMockUser(username = "student", roles = "STUDENT")
+    public void makeReservation_thenReturn201StatusCode() throws Exception {
+
+        Long courseId = createCourse(instructor).getId();
+        ReservationDTO requestBody = ReservationBuilder.createReservationCreationBody(courseId);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mvc.perform(post(CONTROLLER_BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.reservationId", notNullValue()))
+                .andExpect(jsonPath("$.additionalRemarks", is(ADDITIONAL_RESERVATION_REMARKS)))
+                .andExpect(jsonPath("$.accepted", is(Boolean.FALSE)))
+                .andExpect(jsonPath("$.clientId", is(student.getId().intValue())))
+                .andExpect(jsonPath("$.courseId", is(courseId.intValue())));
+    }
+
+    @Test
+    @WithMockUser(username = "student", roles = "STUDENT")
+    public void deleteReservation_thenReturn200StatusCode() throws Exception {
+
+        Reservation reservation = createReservation(Boolean.FALSE);
+
+        mvc.perform(delete(CONTROLLER_BASE_URL.concat("/").concat(String.valueOf(reservation.getId().intValue())))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        assertThat(reservationRepository.count()).isZero();
+    }
+
+    @Test
+    @WithMockUser(username = "instructor", roles = "INSTRUCTOR")
+    public void acceptReservation_thenReturn200StatusCode() throws Exception {
+
+        Reservation reservation = createReservation(Boolean.FALSE);
+
+        mvc.perform(patch(CONTROLLER_BASE_URL.concat("/").concat(String.valueOf(reservation.getId().intValue())))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.reservationId", is(reservation.getId().intValue())))
+                .andExpect(jsonPath("$.additionalRemarks", is(reservation.getAdditionalRemarks())))
+                .andExpect(jsonPath("$.accepted", is(Boolean.TRUE)))
+                .andExpect(jsonPath("$.clientId", is(reservation.getClient().getId().intValue())))
+                .andExpect(jsonPath("$.courseId", is(reservation.getCourse().getId().intValue())));
+    }
+
     private User createStudent() {
 
         Role studentRole = RoleBuilder.createStudentRole();
@@ -117,12 +205,12 @@ public class ReservationRestControllerIntegrationTest {
         return instructor;
     }
 
-    private void createReservation(Boolean accepted) {
+    private Reservation createReservation(Boolean accepted) {
 
         Course course = createCourse(instructor);
 
         Reservation reservation = ReservationBuilder.createDefaultReservation(student, course, accepted);
-        reservationRepository.save(reservation);
+        return reservationRepository.save(reservation);
     }
 
     private Course createCourse(User instructor) {
